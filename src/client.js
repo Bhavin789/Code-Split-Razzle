3bac4dd5b1ad3ab99c3378d3e473a892ec15e049
Bhavin Agarwal <bhavib.789@gmail.com>
2020-08-02 19:14:52 +0530

    Add babel loader plugins and setup SSR with lazy loading using react-loadable, WIP

--- src/client.js
+++ src/client.js
@@ -1,14 +1,17 @@
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import React from 'react';
-import { hydrate } from 'react-dom';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Loadable from "react-loadable";
+import React from "react";
+import { hydrate } from "react-dom";
 
-hydrate(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
-);
+Loadable.preloadReady().then(() => {
+  hydrate(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
+});
 
 if (module.hot) {
   module.hot.accept();
--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-import http from 'http';
+import http from "http";
+import Loadable from "react-loadable";
 
-let app = require('./server').default;
+let app = require("./server").default;
 
 const server = http.createServer(app);
 
 let currentApp = app;
 
-server
-  .listen(process.env.PORT || 3000, () => {
-    console.log('🚀 started');
-  })
-  .on('error', error => {
-    console.log(error);
-  });
+Loadable.preloadAll().then(() => {
+  server
+    .listen(process.env.PORT || 3000, () => {
+      console.log("🚀 started");
+    })
+    .on("error", (error) => {
+      console.log(error);
+    });
+});
 
 if (module.hot) {
-  console.log('✅  Server-side HMR Enabled!');
+  console.log("✅  Server-side HMR Enabled!");
 
-  module.hot.accept('./server', () => {
-    console.log('🔁  HMR Reloading `./server`...');
+  module.hot.accept("./server", () => {
+    console.log("🔁  HMR Reloading `./server`...");
 
     try {
-      app = require('./server').default;
-      server.removeListener('request', currentApp);
-      server.on('request', app);
+      app = require("./server").default;
+      server.removeListener("request", currentApp);
+      server.on("request", app);
       currentApp = app;
     } catch (error) {
       console.error(error);
