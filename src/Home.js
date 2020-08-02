7b402187c060ac4b1af4bac8dcae178f5369dce2
Bhavin Agarwal <bhavib.789@gmail.com>
2020-08-02 19:48:42 +0530

    Apply lazy loading on components, WIP

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import logo from './react.svg';
-import './Home.css';
+import React, { Component } from "react";
+import Loadable from "react-loadable";
+import "./Home.css";
 
-class Home extends React.Component {
+const Intro = Loadable({
+  loader: () => import("./Intro"),
+  loading: () => null,
+});
+
+const Welcome = Loadable({
+  loader: () => import("./Welcome"),
+  loading: () => null,
+});
+
+class Home extends Component {
   render() {
     return (
       <div className="Home">
         <div className="Home-header">
-          <img src={logo} className="Home-logo" alt="logo" />
-          <h2>Welcome to Razzle</h2>
+          <Welcome />
         </div>
-        <p className="Home-intro">
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
+        <Intro />
         <ul className="Home-resources">
           <li>
             <a href="https://github.com/jaredpalmer/razzle">Docs</a>
---
+++ src/Intro.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Intro extends Component {
+  render() {
+    return <div className="Home">This is a lazy loaded component</div>;
+  }
+}
+
+export default Intro;
